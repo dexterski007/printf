d72b47681e3c85192dd02bf2f5513c737d082b81
abderrahmane-elrhassani <abdoghessan@example.com>
2023-11-12 06:59:35 -0800

    test2

--- test2/_printf.c
+++ test2/_printf.c
@@ -65,7 +65,7 @@ int _putchar(char c)
 }
 
 /**
- * print_string - prints a string to stdout
+ * print_string-prints a string to stdout
  * @str: The string to print
  *
  * Return: On success the length of the string.
