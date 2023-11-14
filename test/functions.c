5e1a2b09246ddf3a0b0b6ba015a330f4a47031f2
Dexterski007 <mbouj007@gmail.com>
2023-11-14 12:51:58 -0800

    test

---
+++ test/functions.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * len_count - a function to count integer length
+ *
+ * @n: number
+ *
+ * Return: counted characters
+**/
+
+int len_count(int n)
+
+{
+	if (n == 0)
+	return (0);
+
+	return (1 + len_count(n / 10));
+}
+
+
+/**
+ * print_integer - a function to print integers
+ *
+ * @n: number
+ *
+ * Return: Nothing
+**/
+
+void print_integer(int n)
+{
+	unsigned int r;
+
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		print_integer(147483648);
+		return;
+	}
+
+	if (n < 0)
+	{
+	n = n * -1;
+	_putchar('-');
+	}
+	r = n;
+	r = r / 10;
+	if (r != 0)
+	print_integer(r);
+
+	_putchar((int) n % 10 + '0');
+	return;
+}
